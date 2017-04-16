379b2c0755ae52d58f9bde6da5f3f455766e3e2a
Bruno Secco <contato@brunosecco.com>
2017-04-15 22:59:05 -0300

    crud works

--- src/app/campanha/campanha.component.ts
+++ src/app/campanha/campanha.component.ts
@@ -1,71 +1,29 @@
+import { CrudService } from './../common/crud.service';
 import { CampanhaFilter } from './campanha.filter';
 import { ModalDirective } from 'ng2-bootstrap/modal';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Campanha } from './campanha';
-import { CampanhaService } from './campanha.service';
 
 @Component({
     selector: 'campanha',
     templateUrl: './campanha.component.html',
-    providers: [CampanhaService],
+    providers: [CrudService],
 })
 export class CampanhaComponent implements OnInit {
 
     @ViewChild('createModal') public createModal: ModalDirective;
     @ViewChild('deleteModal') public deleteModal: ModalDirective;
 
-    collectionCampanha: Campanha[];
-    model = new Campanha();
-    filter = new CampanhaFilter();
-
-    constructor(private api: CampanhaService) { 
-        this.api.setResource();
-    }
+    constructor(public crud: CrudService<Campanha, CampanhaFilter>) { }
 
     ngOnInit() {
-        this.Load();
-    }
-
-    Load(filters?: CampanhaFilter) {
-        this.api.Get(filters).subscribe(data => this.collectionCampanha = data.DataList);
-    }
-
-    Filter(){
-        this.Load(this.filter);
-    }
-
-    ConfirmSave() {
-        this.api.Post(this.model).subscribe(data => this.onCreate(data));
-    }
-
-    ConfirmDelete() {
-        this.api.Delete(this.model).subscribe(data => this.onDelete(data));
-    }
-
-    onCreate(data) {
-        this.createModal.hide();
-        this.Load();
-    }
-
-    onDelete(data) {
-        this.createModal.hide();
-        this.Load();
-    }
-
-    edit(id) {
-        const filters = new CampanhaFilter();
-        filters.Id = id;
-        this.api.Get(filters).subscribe(data => this.openModal(data.Data));
-    }
-
-    delete(data: Campanha) {
-        this.model = data;
-        this.deleteModal.show();
-    }
-
-    openModal(data: Campanha) {
-        this.model = data;
-        this.createModal.show();
+        this.crud.Start(
+            'Campanha',
+            new CampanhaFilter(),
+            new Campanha(),
+            this.createModal,
+            this.deleteModal,
+        );
     }
 
 }
--- src/app/common/crud.service.ts
+++ src/app/common/crud.service.ts
@@ -1,12 +1,69 @@
+import { controlPath } from '@angular/forms/src/directives/shared';
+import { ModalDirective } from 'ng2-bootstrap/modal';
+import { Filter } from './filter';
 import { ApiService } from '../common/api.service';
-import { Campanha } from '../campanha/campanha';
-import { Http, RequestOptions, Response, Headers } from '@angular/http';
-import { Inject, Injectable, OnInit } from '@angular/core';
-import { Observable, Observer } from 'rxjs/Rx';
+import { Injectable } from '@angular/core';
 
 @Injectable()
-export class CrudService<T> extends ApiService<T> {
+export class CrudService<T, TFilter extends Filter> extends ApiService<T> {
 
+    public itens: T[];
+    public model: T;
+    public filter: TFilter;
+    public saveModal: ModalDirective;
+    public deleteModal: ModalDirective;
 
+    public Start(resource: string, filter: TFilter, model: T, createModal: ModalDirective, deleteModal: ModalDirective) {
+        this.resource = resource;
+        this.filter = filter;
+        this.model = model;
+        this.saveModal = createModal;
+        this.deleteModal = deleteModal;
+        this.Load();
+    }
 
+    public ExecuteFilter() {
+        this.Load(this.filter);
+    }
+
+    public ExecuteEdit(id) {
+        this.filter.Id = id;
+        this.Get(this.filter).subscribe(data => this.CallbackExecuteEdit(data.Data));
+    }
+
+    public ExecuteCreate() {
+        this.saveModal.show();
+    }
+
+    public ExecuteDelete(data: T) {
+        this.model = data;
+        this.deleteModal.show();
+    }
+
+    public ConfirmSave() {
+        this.Post(this.model).subscribe(data => this.CallbackConfirmSave(data.Data));
+    }
+
+    public ConfirmDelete() {
+        this.Delete(this.model).subscribe(data => this.CallbackConfirmDelete(data.Data));
+    }
+
+    public Load(filters?: TFilter) {
+        this.Get(filters).subscribe(data => this.itens = data.DataList);
+    }
+
+    private CallbackConfirmSave(data: T) {
+        this.saveModal.hide();
+        this.Load();
+    }
+
+    private CallbackConfirmDelete(data: T) {
+        this.deleteModal.hide();
+        this.Load();
+    }
+    private CallbackExecuteEdit(data: T) {
+        this.filter.Id = null;
+        this.model = data;
+        this.saveModal.show();
+    }
 }
